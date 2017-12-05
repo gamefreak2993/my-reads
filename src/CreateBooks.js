2afe4cffbc3f220d63f2f2392348110a6523f674
Mihai Vărșăndan <themike2993@gmail.com>
2017-12-05 13:52:39 +0000

    App now supports books moving shelves.

--- src/CreateBooks.js
+++ src/CreateBooks.js
@@ -1,15 +1,30 @@
 import React,  { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Book from './Book'
 import * as BooksAPI from './utils/BooksAPI';
 
 class CreateBooks extends Component {
   state = {
-    books: []
+    books: [],
+    shelves: {
+      currentlyReading: [],
+      wantToRead: [],
+      read: []
+    }
   }
 
   componentDidMount () {
-    BooksAPI.getAll().then( ( books ) => { this.setState( { books } ) } );
+    BooksAPI.getAll().then( ( books ) => {
+      this.setState( _ => ( {
+        books: books,
+        shelves: {
+          currentlyReading: books.filter( book => book.shelf === 'currentlyReading' ),
+          wantToRead: books.filter( book => book.shelf === 'wantToRead' ),
+          read: books.filter( book => book.shelf === 'read' )
+        }
+      } ) );
+    } );
+
   }
 
   /*static propTypes = {
@@ -17,44 +32,111 @@ class CreateBooks extends Component {
   }*/
 
   render () {
-    let { books } = this.state;
-
-    let bookShelvesSet = new Set();
-    books.map( ( book ) => bookShelvesSet.add( book.shelf ) ); // Get bookshelves for each book and map them to a set
-    let bookShelves = [ ...bookShelvesSet ]; // Convert the set to an array
+    let { shelves } = this.state;
+    let { currentlyReading, wantToRead, read } = this.state.shelves;
+    const shelvesArray = Object.keys( shelves );
 
     return (
       <section className="books">
         <div className="container">
-          { bookShelves.map( ( shelf ) => (
-            <div key={ shelf } className={ `bookShelf ${ shelf }` }>
-              <h4 className="bookShelfTitle">{ shelf.replace(/([A-Z])/g, ' $1') }</h4>
-              <div className="row">
-                { books.filter( book => book.shelf === shelf ).map( ( book ) => (
-                  <Book
-                    key={ book.id }
-                    bookTitle={ book.title }
-                    bookImageLinksThumbnail={ book.imageLinks.thumbnail }
-                    bookPublisher={ book.publisher }
-                    bookPublishedDate={ book.publishedDate }
-                    bookDescription={ book.description }
-                    bookAuthors={ book.authors }
-                    bookShelf={ shelf }
-                    bookShelves={ bookShelves }
-                    currentBook={ book }
-                    onChangeShelf={ ( book, shelf ) => {
-                      BooksAPI.update( book, shelf ).then( _ => {
-                        this.setState( state => ( {
-                          books: state.books.filter( b => b.id !== book.id ).concat( [ book ] )
-                        } ) )
-                      } );
-                    } }
-                  />
-                ) ) }
-              </div>
+          <div className={ "bookShelf" }>
+            <h4 className="bookShelfTitle">Currently reading</h4>
+            <div className="row">
+              { currentlyReading.map( book => (
+                <Book
+                  key={ book.id }
+                  bookTitle={ book.title }
+                  bookImageLinksThumbnail={ book.imageLinks.thumbnail }
+                  bookPublisher={ book.publisher }
+                  bookPublishedDate={ book.publishedDate }
+                  bookDescription={ book.description }
+                  bookAuthors={ book.authors }
+                  bookShelf={ 'currentlyReading' }
+                  bookShelves={ shelvesArray }
+                  currentBook={ book }
+                  onChangeShelf={ ( book, shelf ) => {
+                    BooksAPI.update( book, shelf ).then( _ => {
+                      this.setState( state => ( {
+                        books: state.books,
+                        shelves: {
+                          currentlyReading: state.shelves.currentlyReading.filter( b => b.id !== book.id ),
+                          wantToRead: shelf === 'wantToRead' ? state.shelves.wantToRead.concat( book ) : state.shelves.wantToRead,
+                          read: shelf === 'read' ? state.shelves.read.concat( book ) : state.shelves.read
+                        }
+                      } ) );
+                      console.log( shelf === 'currentlyReading' ? book : 'nothing' );
+                    } );
+                  } }
+                />
+              ) ) }
             </div>
-          ) ) }
-          { console.log( books ) }
+          </div>
+
+          <div className={ "bookShelf" }>
+            <h4 className="bookShelfTitle">Want to read</h4>
+            <div className="row">
+              { wantToRead.map( book => (
+                <Book
+                  key={ book.id }
+                  bookTitle={ book.title }
+                  bookImageLinksThumbnail={ book.imageLinks.thumbnail }
+                  bookPublisher={ book.publisher }
+                  bookPublishedDate={ book.publishedDate }
+                  bookDescription={ book.description }
+                  bookAuthors={ book.authors }
+                  bookShelf={ 'wantToRead' }
+                  bookShelves={ shelvesArray }
+                  currentBook={ book }
+                  onChangeShelf={ ( book, shelf ) => {
+                    BooksAPI.update( book, shelf ).then( _ => {
+                      this.setState( state => ( {
+                        books: state.books,
+                        shelves: {
+                          currentlyReading: shelf === 'currentlyReading' ? state.shelves.currentlyReading.concat( book ) : state.shelves.currentlyReading,
+                          wantToRead: state.shelves.wantToRead.filter( b => b.id !== book.id ),
+                          read: shelf === 'read' ? state.shelves.read.concat( book ) : state.shelves.read
+                        }
+                      } ) );
+                      console.log( shelf === 'wantToRead' ? book : 'nothing' );
+                    } );
+                  } }
+                />
+              ) ) }
+            </div>
+          </div>
+
+          <div className={ "bookShelf" }>
+            <h4 className="bookShelfTitle">read</h4>
+            <div className="row">
+              { read.map( book => (
+                <Book
+                  key={ book.id }
+                  bookTitle={ book.title }
+                  bookImageLinksThumbnail={ book.imageLinks.thumbnail }
+                  bookPublisher={ book.publisher }
+                  bookPublishedDate={ book.publishedDate }
+                  bookDescription={ book.description }
+                  bookAuthors={ book.authors }
+                  bookShelf={ 'read' }
+                  bookShelves={ shelvesArray }
+                  currentBook={ book }
+                  onChangeShelf={ ( book, shelf ) => {
+                    BooksAPI.update( book, shelf ).then( _ => {
+                      this.setState( state => ( {
+                        books: state.books,
+                        shelves: {
+                          currentlyReading: shelf === 'currentlyReading' ? state.shelves.currentlyReading.concat( book ) : state.shelves.currentlyReading,
+                          wantToRead: shelf === 'wantToRead' ? state.shelves.wantToRead.concat( book ) : state.shelves.wantToRead,
+                          read: state.shelves.read.filter( b => b.id !== book.id )
+                        }
+                      } ) );
+                      console.log( shelf === 'read' ? book : 'nothing' );
+                    } );
+                  } }
+                />
+              ) ) }
+            </div>
+          </div>
         </div>
       </section>
     )
